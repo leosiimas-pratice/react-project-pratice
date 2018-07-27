058dc441c6f2cb861864495063b46d1ac8d4d611
leosiimas <abreu.leo@hotmail.com>
2018-07-27 18:44:48 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+// ExtractTextPlugin foi adicionado no ex.10
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './ex/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+}
