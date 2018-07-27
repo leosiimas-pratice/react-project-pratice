2839e1ef91a0eb285f42c45650bbb3743873ec47
leosiimas <abreu.leo@hotmail.com>
2018-07-27 18:50:29 -0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack')
 // ExtractTextPlugin foi adicionado no ex.10
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './ex/index.js',
