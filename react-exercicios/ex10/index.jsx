4d2c5fd76555d83158edfeaabb56553f8940044f
leosiimas <abreu.leo@hotmail.com>
2018-07-28 15:30:38 -0300

    feat react-full

---
+++ react-exercicios/ex10/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CLassComponent from './classComponent'
+
+ReactDOM.render(
+  <CLassComponent label='COntador' initialValue={10}/>
+, document.getElementById('app'))
---
+++ react-exercicios/ex9/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CLassComponent from './classComponent'
+
+ReactDOM.render(
+  <CLassComponent value='Componente Class'/>
+, document.getElementById('app'))
