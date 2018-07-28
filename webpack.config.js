1ba4da7654a1d36110585613ebcb9cf51449077c
leosiimas <abreu.leo@hotmail.com>
2018-07-27 22:19:18 -0300

    feat webpack-full

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
-// ExtractTextPlugin foi adicionado no ex.10
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './ex/index.js',
@@ -11,4 +12,27 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
+
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+
+                presets: ['es2015', 'react'],
+
+                plugins: ['transform-object-rest-spread']
+            }
+        },
+
+        {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        }]
+    }
 }
